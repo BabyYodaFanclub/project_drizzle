6fe5a042229195a80f5c954030eff1a305127a5b
Luca Nixdorf <luca@familie-nixdorf.de>
2020-03-02 22:14:13 +0100

    Adressables should now be built in the github action

--- ProjectDrizzle/Assets/Scripts/Editor/BuildAddressablesProcessor.cs
+++ ProjectDrizzle/Assets/Scripts/Editor/BuildAddressablesProcessor.cs
@@ -1,8 +1,6 @@
 ﻿using UnityEditor;
 using UnityEditor.AddressableAssets;
 using UnityEditor.AddressableAssets.Settings;
-using UnityEngine;
-using System.Collections;
 
 /// <summary>
 /// The script gives you choice to whether to build addressable bundles when clicking the build button.
@@ -10,7 +8,7 @@ using System.Collections;
 /// For cloud build, put BuildAddressablesProcessor.PreExport as PreExport command.
 /// Discussion: https://forum.unity.com/threads/how-to-trigger-build-player-content-when-build-unity-project.689602/
 /// </summary>
-class BuildAddressablesProcessor
+static class BuildAddressablesProcessor
 {
     [InitializeOnLoadMethod]
     private static void Initialize()
@@ -20,10 +18,7 @@ class BuildAddressablesProcessor
 
     private static void BuildPlayerHandler(BuildPlayerOptions options)
     {
-        //if (EditorUtility.DisplayDialog("Build with Addressables", "Do you want to build a clean addressables before export?", "Build with Addressables", "Skip"))
-        //{
         PreExport();
-        //}
         BuildPlayerWindow.DefaultBuildMethods.BuildPlayer(options);
     }
     
@@ -32,9 +27,7 @@ class BuildAddressablesProcessor
     /// </summary>
     public static void PreExport()
     {
-        //Debug.Log("BuildAddressablesProcessor.PreExport start");
         AddressableAssetSettings.CleanPlayerContent(AddressableAssetSettingsDefaultObject.Settings.ActivePlayerDataBuilder);
         AddressableAssetSettings.BuildPlayerContent();
-        //Debug.Log("BuildAddressablesProcessor.PreExport done");
     }
 }
