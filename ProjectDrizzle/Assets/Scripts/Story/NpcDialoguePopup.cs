bb4e0da20fd4dfbb317d378fe15b6a3917b37810
Luca Nixdorf <luca@familie-nixdorf.de>
2020-03-18 22:08:59 +0100

    "press E to chat" thing added

---
+++ ProjectDrizzle/Assets/Scripts/Story/NpcDialoguePopup.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDialoguePopup : MonoBehaviour
+{
+    public Canvas DialogueCanvas;
+    private bool _playerInRange;
+    private bool _dialoguePlaying;
+
+    private void Update()
+    {
+        if (DialogueCanvas.gameObject.activeSelf)
+            if (!_playerInRange && !_dialoguePlaying)
+                DialogueCanvas.gameObject.SetActive(false);
+        
+        if (!DialogueCanvas.gameObject.activeSelf)
+            if (_playerInRange)
+                DialogueCanvas.gameObject.SetActive(true);
+
+        if (_playerInRange && !_dialoguePlaying && Input.GetButtonUp("Interact"))
+            StartDialogue();
+    }
+
+    private void StartDialogue()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _playerInRange = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        _playerInRange = false;
+    }
+}
